28c03fec3882dd001e63f6c461abb93818590073
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-15 16:48:27 +0100

    ajout du model service

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_152009) do
+ActiveRecord::Schema.define(version: 2021_11_15_154618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "speciality"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_11_15_152009) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "services", "users"
 end
