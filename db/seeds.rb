32ae925be205177cd4f484e59d6d8ed5d3a0a765
Victor Chomette <victorchomette@hotmail.fr>
2021-11-16 17:10:03 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Service.destroy_all
 User.destroy_all
 
 User.create(email: 'blalba@gmail.com', password: '123456', first_name: 'Kanye', last_name: 'West',
